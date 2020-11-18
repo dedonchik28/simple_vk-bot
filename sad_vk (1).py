5eb803ad7777caff8d05eb53789aa43dfdd764f3
dedonchik28 <74668015+dedonchik28@users.noreply.github.com>
2020-11-18 19:45:09 +0400

    Update sad_vk (1).py

--- sad_vk (1).py
+++ sad_vk (1).py
@@ -5,22 +5,17 @@ key = "be798d82703344fac674442408d2b87b8d781425e22541dc3c78475e179a1d068bb1b01a1
 vk = vk_api.VkApi(token=key)
 
 def send_message(user_id, message):
-                from random import randint
-                vk.method('messages.send',
-                          {'user_id': user_id,
-                           "random_id":randint(1,1000) ,
-                           'message': message,}
-                          )
-
+  from random import randint
+  vk.method('messages.send',{'user_id': user_id,"random_id":randint(1,1000) ,'message': message,})
 # Работа с сообщениями
 longpoll = VkLongPoll(vk)
 # Основной цикл
 for event in longpoll.listen():
     # Если пришло новое сообщение
-    if event.type == VkEventType.MESSAGE_NEW:
+  if event.type == VkEventType.MESSAGE_NEW:
         # Если оно имеет метку для меня( то есть бота)
-        if event.to_me:
-            text = event.text.lower()
-            user_id = event.user_id
-              if text == "привет":
-                send(user_id, "куку")
+    if event.to_me:
+      text = event.text.lower()
+      user_id = event.user_id
+        if text == "привет":
+          send(user_id, "куку")
